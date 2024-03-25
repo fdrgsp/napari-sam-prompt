32c4d7323f7e3728efba8974eb63fea96a7fa097
fdrgsp <federico.gasparoli@gmail.com>
2024-03-24 21:52:10 -0400

    fix: init

--- src/napari_sam_prompt/__init__.py
+++ src/napari_sam_prompt/__init__.py
@@ -4,3 +4,7 @@ try:
     from ._version import version as __version__
 except ImportError:
     __version__ = "unknown"
+
+from ._sam_prompt_widget import SamPromptWidget
+
+__all__ = ["__version__", "SamPromptWidget"]
