7bed0a2c3db406aa671988efb73716005ed5e255
Ranit Karmakar <rkarmaka@mtu.edu>
2024-03-29 21:27:15 -0400

    Update _sam_prompt_widget.py

--- src/napari_sam_prompt/_sam_prompt_widget.py
+++ src/napari_sam_prompt/_sam_prompt_widget.py
@@ -4,6 +4,8 @@ import contextlib
 import logging
 from typing import TYPE_CHECKING, Any, Generator, cast
 
+import cv2 as cv
+
 import napari.layers
 import napari.viewer
 import numpy as np
@@ -272,31 +274,50 @@ class SamPromptWidget(QWidget):
 
         return data_8bit.astype('uint8')
 
+
     def _convert_8bit(self, data):
         '''Convert image to 8-bit'''
-        for i in range(3):
-            temp_data = data[:,:,i]
-            data[:,:,i] = (255*((temp_data-temp_data.min())/(temp_data.max()-temp_data.min()))).astype('uint8')
+        data = (255*((data-data.min())/(data.max()-data.min()))).astype('uint8')
         return data
 
 
     def _convert_to_three_channels(self, data):
         '''Convert image to 3-channel image'''
         if len(data.shape)==3:
-            print('The image has 3 channels')
+            # Convert the image from CHW to HWC dimension
+            ch_loc = np.argmin(data.shape)
+            if ch_loc==0:
+                data=np.transpose(data, (1, 2, 0))
+            
             channels = data.shape[-1]
         else:
             channels = 1
 
+        data_8bit = np.zeros_like(data, dtype='uint8')
+            
         if channels==1:
-            return np.stack([data] * 3, axis=-1)
+            data_8bit = self._convert_8bit(data)
+            data_8bit = cv.equalizeHist(data_8bit)
+            return np.stack([data_8bit] * 3, axis=-1)
         elif channels==2:
-            avg_channels = np.mean(data, axis=-1, keepdims=True)
-            return np.concatenate([data, avg_channels], axis=-1)
+            for i in range(channels):
+                temp_data = data[:,:,i]
+                temp_data = self._convert_8bit(temp_data)
+                data_8bit[:,:,i] = cv.equalizeHist(temp_data)
+            avg_channels = np.mean(data_8bit, axis=-1, dtype='uint8')
+            return np.stack([data_8bit[:,:,0], data_8bit[:,:,1], avg_channels], axis=-1)
         elif channels==3:
-            return data
+            for i in range(channels):
+                temp_data = data[:,:,i]
+                temp_data = self._convert_8bit(temp_data)
+                data_8bit[:,:,i] = cv.equalizeHist(temp_data)
+            return data_8bit
         else:
-            avg_channels = np.mean(data, axis=-1, keepdims=True)
+            for i in range(channels):
+                temp_data = data[:,:,i]
+                temp_data = self._convert_8bit(temp_data)
+                data_8bit[:,:,i] = cv.equalizeHist(temp_data)
+            avg_channels = np.mean(data_8bit, axis=-1, dtype='uint8')
             return np.stack([avg_channels] * 3, axis=-1)
 
     # ========================MODEL=========================
